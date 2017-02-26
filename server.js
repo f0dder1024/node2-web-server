fe947ce4c0ae17ee2fb866183093aa780263e3d6
f0dder1024 <mlee1024@gmail.com>
2017-02-25 22:13:28 -0800

    Add projects

--- server.js
+++ server.js
@@ -52,6 +52,13 @@ app.get('/about', (req, res)=>{
     });
 })
 
+app.get('/projects', (req, res)=>{
+    res.render('projects.hbs',{
+        pageTitle: 'Projects Page',
+        projectName: 'Butt Stuff'
+    });
+})
+
 app.get('/bad', (req, res)=>{
     res.send({
         error: 'Error, bad route encountered'
